777ba6d71c8a0fda6bdd8ee0123b9367e46855ac
bbush915 <bushb@umich.edu>
2022-11-25 18:20:43 -0600

    feat: Adds a memoize helper.

--- src/utils/common.js
+++ src/utils/common.js
@@ -8,6 +8,31 @@ function clone(obj) {
   return JSON.parse(JSON.stringify(obj));
 }
 
+/**
+ * Memoizes a function
+ *
+ * @param {Function} func The function.
+ * @return {Function} The memoized function.
+ */
+function memoize(func) {
+  const lookup = new Map();
+
+  return function () {
+    const key = [...arguments].map((x) => String(x)).join("|");
+
+    if (lookup.has(key)) {
+      return lookup.get(key);
+    }
+
+    const result = func(...arguments);
+
+    lookup.set(key, result);
+
+    return result;
+  };
+}
+
 module.exports = {
   clone,
+  memoize,
 };
