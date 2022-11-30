f6fcf4ab098eed2c2b03bacf93e70a4f5978e1f2
bbush915 <bushb@umich.edu>
2022-11-30 17:12:20 -0600

    feat: Do basic parsing in template.

--- src/day.template.ts
+++ src/day.template.ts
@@ -1,7 +1,12 @@
 import fs from "fs";
 
 function parseInput() {
-  return fs.readFileSync("src/day.$$DAY$$.input.txt");
+  return fs
+    .readFileSync("src/day.$$DAY$$.input.txt")
+    .toString()
+    .split("\n")
+    .filter((x) => x)
+    .map((x) => x);
 }
 
 export function part1() {
